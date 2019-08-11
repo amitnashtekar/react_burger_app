be47335f711004de0a89d40951f2819159bf283c
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2019-08-11 21:57:43 +1000

    msg please add ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -9,11 +9,15 @@ const burger = (props) => {
     let transformedIngredients = Object.keys( props.ingredients ).map((ingredientKey) => {
         //a new array with n number of indexes is created based on the value held by the key in the 'transformedIngredients' array
         return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
-          //
-          console.log(_,i);
-          return <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />
+
+            return <BurgerIngredients key={ingredientKey + i} type={ingredientKey} />
         })
-      })
+      }).reduce((arr,el)=>{
+        return arr.concat(el)
+      },[])
+      if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please add ingredients!</p> 
+      }
     return(
     <div className={classes.Burger} >
     <BurgerIngredients type="bread-top"></BurgerIngredients>
