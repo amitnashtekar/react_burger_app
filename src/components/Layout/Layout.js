b836a7ae6c9cf235f5db3efc34a4aa0868626030
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2019-08-14 22:13:52 +1000

    basic toolbar created

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Auxi from '../../hoc/Auxi';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 const layout = (props) => (
     <Auxi>
-        <div>Toolbar , SideDrawer, BackDrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
