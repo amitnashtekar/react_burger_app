4911c142ef39e778b9623c1e52f29cf0a0ab0b9c
Amit Ashtekar <Amit.Ashtekar@amdocs.com>
2019-08-25 15:29:25 +1000

    checkout container added

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const CustomerSummary = props => {
+    return (
+        <div className={classes.CheckoutSummary}>
+                <div>Here is your Order to checkout!! Enjoy !!</div> 
+                <div>
+                    <Burger ingredients={props.ingredients}/>
+                    <Button onClick={props.cancelOrder} type ='Danger'>Cancel</Button>
+                    <Button onClick = {props.purchaseContinue} type ='Success'>Continue</Button>
+                </div>
+        </div>
+    )
+}
+export default CustomerSummary
